176dbe9d89233d485574568515f6cfca4add86df
Marco Locks <marcolocks@gmail.com>
2022-06-15 18:26:49 -0300

    projeto inicial

---
+++ app/src/main/java/com/how_vi/autoescola/SecondFragment.java
@@ -0,0 +1,62 @@
+package com.how_vi.autoescola;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import com.how_vi.autoescola.databinding.FragmentSecondBinding;
+
+public class SecondFragment extends Fragment {
+
+private FragmentSecondBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+      binding = FragmentSecondBinding.inflate(inflater, container, false);
+      return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu){
+        MenuItem item = menu.findItem(R.id.action_cadastros);
+        if(item != null)
+            item.setVisible(false);
+    }
+
+
+}
